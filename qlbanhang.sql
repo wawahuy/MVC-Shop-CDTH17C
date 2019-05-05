9d1638f25c619cdae9cafa514f74aaf452fad677
wawahuy <kakahuy99@gmail.com>
2019-05-05 22:33:47 +0700

    CSDL no constraint

--- qlbanhang.sql
+++ qlbanhang.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th4 04, 2019 lúc 01:50 AM
--- Phiên bản máy phục vụ: 10.1.38-MariaDB
--- Phiên bản PHP: 7.3.2
+-- Máy chủ: 127.0.0.1:3306
+-- Thời gian đã tạo: Th5 05, 2019 lúc 02:15 PM
+-- Phiên bản máy phục vụ: 5.7.24
+-- Phiên bản PHP: 7.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,79 +25,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `admin`
---
-
-CREATE TABLE `admin` (
-  `username` varchar(32) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  `fullname` varchar(200) NOT NULL,
-  `birthday` date NOT NULL,
-  `age` int(11) NOT NULL,
-  `joindate` date NOT NULL,
-  `salary` double NOT NULL,
-  `id_competence` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Cấu trúc bảng cho bảng `categories`
 --
 
-CREATE TABLE `categories` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `deltail` varchar(5000) COLLATE utf8_unicode_ci NOT NULL,
-  `image` varchar(10000) COLLATE utf8_unicode_ci NOT NULL,
-  `root` bit(1) DEFAULT b'0'
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE IF NOT EXISTS `categories` (
+  `categorie_id` int(11) NOT NULL AUTO_INCREMENT,
+  `categorie_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `categorie_image` text COLLATE utf8_unicode_ci NOT NULL,
+  `categorie_deltail` text COLLATE utf8_unicode_ci NOT NULL,
+  `categorie_parent` int(11) NOT NULL,
+  `employee_id` int(11) NOT NULL,
+  PRIMARY KEY (`categorie_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Đang đổ dữ liệu cho bảng `categories`
---
-
-INSERT INTO `categories` (`id`, `name`, `deltail`, `image`, `root`) VALUES
-(1, 'Riêng Nữ', 'Phong cách thời trang tối giản\r\nNhiều sự lựa chọn\r\nGiá thành hợp lí', 'Resource/upload/girl.jpg', b'1'),
-(2, 'Riêng Nam', 'Phong cách thời trang tối giản\r\nNhiều sự lựa chọn\r\nGiá thành hợp lí', 'Resource/upload/men.jpg', b'1'),
-(3, 'Trẻ em', 'Phong cách thời trang tối giản\r\nNhiều sự lựa chọn\r\nGiá thành hợp lí', 'Resource/upload/child.jpg', b'1'),
-(4, 'Phụ kiện', 'Phong cách thời trang tối giản\r\nNhiều sự lựa chọn\r\nGiá thành hợp lí', 'Resource/upload/phukien.jpg', b'1'),
-(5, 'Áo khoát', '', '', b'0'),
-(6, 'Áo thun', '', '', b'0'),
-(7, 'Quần shot', '', '', b'0'),
-(8, 'Kính', '', '', b'0');
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `categories_child`
+-- Cấu trúc bảng cho bảng `comments`
 --
 
-CREATE TABLE `categories_child` (
-  `id_parent` int(11) NOT NULL,
-  `id_child` int(11) NOT NULL
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE IF NOT EXISTS `comments` (
+  `comment_id` int(11) NOT NULL AUTO_INCREMENT,
+  `comment_content` text COLLATE utf8_unicode_ci NOT NULL,
+  `comment_date` datetime NOT NULL,
+  `comment_parent` int(11) NOT NULL,
+  `member_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  PRIMARY KEY (`comment_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Đang đổ dữ liệu cho bảng `categories_child`
---
-
-INSERT INTO `categories_child` (`id_parent`, `id_child`) VALUES
-(1, 6),
-(2, 5),
-(3, 7),
-(4, 8);
-
--- --------------------------------------------------------
-
---
--- Cấu trúc bảng cho bảng `competence`
---
-
-CREATE TABLE `competence` (
-  `id` int(11) NOT NULL,
-  `name` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -105,19 +62,46 @@ CREATE TABLE `competence` (
 -- Cấu trúc bảng cho bảng `config`
 --
 
-CREATE TABLE `config` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `data` text COLLATE utf8_unicode_ci NOT NULL
+DROP TABLE IF EXISTS `config`;
+CREATE TABLE IF NOT EXISTS `config` (
+  `config_id` int(11) NOT NULL AUTO_INCREMENT,
+  `config_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `config_data` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`config_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `contacts`
+--
+
+DROP TABLE IF EXISTS `contacts`;
+CREATE TABLE IF NOT EXISTS `contacts` (
+  `contact_id` int(11) NOT NULL AUTO_INCREMENT,
+  `contact_address` text COLLATE utf8_unicode_ci NOT NULL,
+  `contact_phone` int(11) NOT NULL,
+  `member_id` int(11) NOT NULL,
+  PRIMARY KEY (`contact_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
+
 --
--- Đang đổ dữ liệu cho bảng `config`
+-- Cấu trúc bảng cho bảng `employees`
 --
 
-INSERT INTO `config` (`id`, `name`, `data`) VALUES
-(4, 'categories', '[{\"id\":\"1\",\"name\":\"Ri\\u00eang N\\u1eef\",\"deltail\":\"Phong c\\u00e1ch th\\u1eddi trang t\\u1ed1i gi\\u1ea3n\\r\\nNhi\\u1ec1u s\\u1ef1 l\\u1ef1a ch\\u1ecdn\\r\\nGi\\u00e1 th\\u00e0nh h\\u1ee3p l\\u00ed\",\"image\":\"Resource\\/upload\\/girl.jpg\",\"child\":[{\"id\":\"6\",\"name\":\"\\u00c1o thun\",\"deltail\":\"\",\"image\":\"\"}]},{\"id\":\"2\",\"name\":\"Ri\\u00eang Nam\",\"deltail\":\"Phong c\\u00e1ch th\\u1eddi trang t\\u1ed1i gi\\u1ea3n\\r\\nNhi\\u1ec1u s\\u1ef1 l\\u1ef1a ch\\u1ecdn\\r\\nGi\\u00e1 th\\u00e0nh h\\u1ee3p l\\u00ed\",\"image\":\"Resource\\/upload\\/men.jpg\",\"child\":[{\"id\":\"5\",\"name\":\"\\u00c1o kho\\u00e1t\",\"deltail\":\"\",\"image\":\"\"}]},{\"id\":\"3\",\"name\":\"Tr\\u1ebb em\",\"deltail\":\"Phong c\\u00e1ch th\\u1eddi trang t\\u1ed1i gi\\u1ea3n\\r\\nNhi\\u1ec1u s\\u1ef1 l\\u1ef1a ch\\u1ecdn\\r\\nGi\\u00e1 th\\u00e0nh h\\u1ee3p l\\u00ed\",\"image\":\"Resource\\/upload\\/child.jpg\",\"child\":[{\"id\":\"7\",\"name\":\"Qu\\u1ea7n shot\",\"deltail\":\"\",\"image\":\"\",\"child\":[{\"id\":\"8\",\"name\":\"K\\u00ednh\",\"deltail\":\"\",\"image\":\"\"}]}]},{\"id\":\"4\",\"name\":\"Ph\\u1ee5 ki\\u1ec7n\",\"deltail\":\"Phong c\\u00e1ch th\\u1eddi trang t\\u1ed1i gi\\u1ea3n\\r\\nNhi\\u1ec1u s\\u1ef1 l\\u1ef1a ch\\u1ecdn\\r\\nGi\\u00e1 th\\u00e0nh h\\u1ee3p l\\u00ed\",\"image\":\"Resource\\/upload\\/phukien.jpg\"}]'),
-(5, 'slides', '[\"Resource/upload/bn1.jpg\", \"Resource/upload/bn2.jpg\", \"Resource/upload/bn5.jpg\"]');
+DROP TABLE IF EXISTS `employees`;
+CREATE TABLE IF NOT EXISTS `employees` (
+  `employee_id` int(11) NOT NULL AUTO_INCREMENT,
+  `employee_user` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `employee_pass` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `employee_fullname` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `employee_phone` varchar(12) COLLATE utf8_unicode_ci NOT NULL,
+  `employee_email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `employee_birth` date NOT NULL,
+  PRIMARY KEY (`employee_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -125,156 +109,105 @@ INSERT INTO `config` (`id`, `name`, `data`) VALUES
 -- Cấu trúc bảng cho bảng `members`
 --
 
-CREATE TABLE `members` (
-  `id` int(11) NOT NULL,
-  `username` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(2048) COLLATE utf8_unicode_ci NOT NULL,
-  `fullname` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `phone` int(11) NOT NULL,
-  `address` text COLLATE utf8_unicode_ci NOT NULL,
-  `indentity_card` int(11) NOT NULL
+DROP TABLE IF EXISTS `members`;
+CREATE TABLE IF NOT EXISTS `members` (
+  `member_id` int(11) NOT NULL AUTO_INCREMENT,
+  `member_user` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `member_pass` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `member_fullname` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `member_phone` int(11) NOT NULL,
+  `member_email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `member_birth` date NOT NULL,
+  `member_sex` tinyint(1) NOT NULL,
+  PRIMARY KEY (`member_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Đang đổ dữ liệu cho bảng `members`
---
-
-INSERT INTO `members` (`id`, `username`, `password`, `email`, `fullname`, `phone`, `address`, `indentity_card`) VALUES
-(3, 'admin', 'adadad', 'kakahuy99@gmail.com', 'Huy Nguyen', 1697777777, 'Ahihi, Lazaba, Kakaka', 123456789);
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `product`
+-- Cấu trúc bảng cho bảng `orders`
 --
 
-CREATE TABLE `product` (
-  `id` int(11) NOT NULL,
-  `name` text COLLATE utf8_unicode_ci NOT NULL,
-  `star` tinyint(4) NOT NULL,
-  `sale` int(11) NOT NULL,
-  `note` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
-  `view` int(11) NOT NULL,
-  `num_current` int(11) NOT NULL,
-  `num_sold` int(11) NOT NULL,
-  `image` mediumtext COLLATE utf8_unicode_ci NOT NULL,
-  `deltail` text COLLATE utf8_unicode_ci NOT NULL,
-  `price` int(11) NOT NULL,
-  `categories_id` int(11) NOT NULL,
-  `json_option` text COLLATE utf8_unicode_ci NOT NULL
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE IF NOT EXISTS `orders` (
+  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_status` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `order_date` datetime NOT NULL,
+  `member_id` int(11) NOT NULL,
+  `employee_id` int(11) NOT NULL,
+  PRIMARY KEY (`order_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Đang đổ dữ liệu cho bảng `product`
---
-
-INSERT INTO `product` (`id`, `name`, `star`, `sale`, `note`, `view`, `num_current`, `num_sold`, `image`, `deltail`, `price`, `categories_id`, `json_option`) VALUES
-(1, 'Fake', 4, 20, 'New', 1000, 100, 21, '[\"Resource/upload/ao-so-mi-hong.jpg\"]', 'Fake deltail!', 300000, 6, '{\r\n    \"Phân loại\": [\"A\", \"B\"],\r\n    \"Kích thước\": [\"X\", \"XL\"]\r\n    }'),
-(2, 'Fake 2', 4, 0, 'New', 2000, 1300, 211, '[\"Resource/upload/ao-tre-vai-phoi-mau.jpg\"]', 'Fake deltail!', 200000, 5, '{\r\n    \"Phân loại\": [\"A\", \"B\"],\r\n    \"Kích thước\": [\"X\", \"XL\"]\r\n    }'),
-(3, 'Fake', 4, 20, 'New', 1000, 100, 21, '[\"Resource/upload/ao-so-mi-hong.jpg\"]', 'Fake deltail!', 300000, 6, '{\r\n    \"Phân loại\": [\"A\", \"B\"],\r\n    \"Kích thước\": [\"X\", \"XL\"]\r\n    }'),
-(4, 'Fake', 4, 20, 'New', 1000, 100, 21, '[\"Resource/upload/ao-so-mi-hong.jpg\"]', 'Fake deltail!', 300000, 6, '{\r\n    \"Phân loại\": [\"A\", \"B\"],\r\n    \"Kích thước\": [\"X\", \"XL\"]\r\n    }'),
-(5, 'Fake', 4, 20, 'New', 1000, 100, 21, '[\"Resource/upload/ao-so-mi-hong.jpg\"]', 'Fake deltail!', 300000, 6, '{\r\n    \"Phân loại\": [\"A\", \"B\"],\r\n    \"Kích thước\": [\"X\", \"XL\"]\r\n    }'),
-(6, 'Fake', 4, 20, 'New', 1000, 100, 21, '[\"Resource/upload/ao-so-mi-hong.jpg\"]', 'Fake deltail!', 300000, 6, '{\r\n    \"Phân loại\": [\"A\", \"B\"],\r\n    \"Kích thước\": [\"X\", \"XL\"]\r\n    }'),
-(7, 'Fake 2', 4, 0, 'New', 2000, 1300, 211, '[\"Resource/upload/ao-tre-vai-phoi-mau.jpg\"]', 'Fake deltail!', 200000, 5, '{\r\n    \"Phân loại\": [\"A\", \"B\"],\r\n    \"Kích thước\": [\"X\", \"XL\"]\r\n    }'),
-(8, 'Fake', 4, 20, 'New', 1000, 100, 21, '[\"Resource/upload/ao-so-mi-hong.jpg\"]', 'Fake deltail!', 300000, 6, '{\r\n    \"Phân loại\": [\"A\", \"B\"],\r\n    \"Kích thước\": [\"X\", \"XL\"]\r\n    }'),
-(9, 'Fake', 4, 20, 'New', 1000, 100, 21, '[\"Resource/upload/ao-so-mi-hong.jpg\"]', 'Fake deltail!', 300000, 6, '{\r\n    \"Phân loại\": [\"A\", \"B\"],\r\n    \"Kích thước\": [\"X\", \"XL\"]\r\n    }'),
-(10, 'Fake', 4, 20, 'New', 1000, 100, 21, '[\"Resource/upload/ao-so-mi-hong.jpg\"]', 'Fake deltail!', 300000, 6, '{\r\n    \"Phân loại\": [\"A\", \"B\"],\r\n    \"Kích thước\": [\"X\", \"XL\"]\r\n    }'),
-(11, 'Fake', 4, 20, 'New', 1000, 100, 21, '[\"Resource/upload/ao-so-mi-hong.jpg\"]', 'Fake deltail!', 300000, 6, '{\r\n    \"Phân loại\": [\"A\", \"B\"],\r\n    \"Kích thước\": [\"X\", \"XL\"]\r\n    }'),
-(12, 'Fake 2', 4, 0, 'New', 2000, 1300, 211, '[\"Resource/upload/ao-tre-vai-phoi-mau.jpg\"]', 'Fake deltail!', 200000, 5, '{\r\n    \"Phân loại\": [\"A\", \"B\"],\r\n    \"Kích thước\": [\"X\", \"XL\"]\r\n    }'),
-(13, 'Fake', 4, 20, 'New', 1000, 100, 21, '[\"Resource/upload/ao-so-mi-hong.jpg\"]', 'Fake deltail!', 300000, 6, '{\r\n    \"Phân loại\": [\"A\", \"B\"],\r\n    \"Kích thước\": [\"X\", \"XL\"]\r\n    }'),
-(14, 'Fake', 4, 20, 'New', 1000, 100, 21, '[\"Resource/upload/ao-so-mi-hong.jpg\"]', 'Fake deltail!', 300000, 6, '{\r\n    \"Phân loại\": [\"A\", \"B\"],\r\n    \"Kích thước\": [\"X\", \"XL\"]\r\n    }'),
-(15, 'Fake', 4, 20, 'New', 1000, 100, 21, '[\"Resource/upload/ao-so-mi-hong.jpg\"]', 'Fake deltail!', 300000, 6, '{\r\n    \"Phân loại\": [\"A\", \"B\"],\r\n    \"Kích thước\": [\"X\", \"XL\"]\r\n    }'),
-(16, 'Fake', 4, 20, 'New', 1000, 100, 21, '[\"Resource/upload/ao-so-mi-hong.jpg\"]', 'Fake deltail!', 300000, 6, '{\r\n    \"Phân loại\": [\"A\", \"B\"],\r\n    \"Kích thước\": [\"X\", \"XL\"]\r\n    }'),
-(17, 'Fake 2', 4, 0, 'New', 2000, 1300, 211, '[\"Resource/upload/ao-tre-vai-phoi-mau.jpg\"]', 'Fake deltail!', 200000, 5, '{\r\n    \"Phân loại\": [\"A\", \"B\"],\r\n    \"Kích thước\": [\"X\", \"XL\"]\r\n    }'),
-(18, 'Fake', 4, 20, 'New', 1000, 100, 21, '[\"Resource/upload/ao-so-mi-hong.jpg\"]', 'Fake deltail!', 300000, 6, '{\r\n    \"Phân loại\": [\"A\", \"B\"],\r\n    \"Kích thước\": [\"X\", \"XL\"]\r\n    }'),
-(19, 'Fake', 4, 20, 'New', 1000, 100, 21, '[\"Resource/upload/ao-so-mi-hong.jpg\"]', 'Fake deltail!', 300000, 6, '{\r\n    \"Phân loại\": [\"A\", \"B\"],\r\n    \"Kích thước\": [\"X\", \"XL\"]\r\n    }'),
-(20, 'Fake', 4, 20, 'New', 1000, 100, 21, '[\"Resource/upload/ao-so-mi-hong.jpg\"]', 'Fake deltail!', 300000, 6, '{\r\n    \"Phân loại\": [\"A\", \"B\"],\r\n    \"Kích thước\": [\"X\", \"XL\"]\r\n    }'),
-(21, 'Fake', 4, 20, 'New', 1000, 100, 21, '[\"Resource/upload/ao-so-mi-hong.jpg\"]', 'Fake deltail!', 300000, 6, '{\r\n    \"Phân loại\": [\"A\", \"B\"],\r\n    \"Kích thước\": [\"X\", \"XL\"]\r\n    }'),
-(22, 'Fake 2', 4, 0, 'New', 2000, 1300, 211, '[\"Resource/upload/ao-tre-vai-phoi-mau.jpg\"]', 'Fake deltail!', 200000, 5, '{\r\n    \"Phân loại\": [\"A\", \"B\"],\r\n    \"Kích thước\": [\"X\", \"XL\"]\r\n    }'),
-(23, 'Fake', 4, 20, 'New', 1000, 100, 21, '[\"Resource/upload/ao-so-mi-hong.jpg\"]', 'Fake deltail!', 300000, 6, '{\r\n    \"Phân loại\": [\"A\", \"B\"],\r\n    \"Kích thước\": [\"X\", \"XL\"]\r\n    }'),
-(24, 'Fake', 4, 20, 'New', 1000, 100, 21, '[\"Resource/upload/ao-so-mi-hong.jpg\"]', 'Fake deltail!', 300000, 6, '{\r\n    \"Phân loại\": [\"A\", \"B\"],\r\n    \"Kích thước\": [\"X\", \"XL\"]\r\n    }'),
-(25, 'Fake', 4, 20, 'New', 1000, 100, 21, '[\"Resource/upload/ao-so-mi-hong.jpg\"]', 'Fake deltail!', 300000, 6, '{\r\n    \"Phân loại\": [\"A\", \"B\"],\r\n    \"Kích thước\": [\"X\", \"XL\"]\r\n    }');
+-- --------------------------------------------------------
 
 --
--- Chỉ mục cho các bảng đã đổ
+-- Cấu trúc bảng cho bảng `order_elements`
 --
 
---
--- Chỉ mục cho bảng `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`username`);
+DROP TABLE IF EXISTS `order_elements`;
+CREATE TABLE IF NOT EXISTS `order_elements` (
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `order_ele_price` decimal(10,0) NOT NULL,
+  `order_ele_num` int(11) NOT NULL,
+  `order_ele_options` int(11) NOT NULL,
+  PRIMARY KEY (`order_id`,`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
 
 --
--- Chỉ mục cho bảng `categories`
---
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
-
---
--- Chỉ mục cho bảng `categories_child`
---
-ALTER TABLE `categories_child`
-  ADD PRIMARY KEY (`id_parent`,`id_child`);
-
---
--- Chỉ mục cho bảng `competence`
---
-ALTER TABLE `competence`
-  ADD PRIMARY KEY (`id`);
-
---
--- Chỉ mục cho bảng `config`
---
-ALTER TABLE `config`
-  ADD PRIMARY KEY (`id`);
-
---
--- Chỉ mục cho bảng `members`
---
-ALTER TABLE `members`
-  ADD PRIMARY KEY (`id`);
-
---
--- Chỉ mục cho bảng `product`
---
-ALTER TABLE `product`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT cho các bảng đã đổ
+-- Cấu trúc bảng cho bảng `policy`
 --
 
---
--- AUTO_INCREMENT cho bảng `categories`
---
-ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+DROP TABLE IF EXISTS `policy`;
+CREATE TABLE IF NOT EXISTS `policy` (
+  `policy_id` int(11) NOT NULL AUTO_INCREMENT,
+  `policy_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `policy_symbol` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `policy_parent` int(11) NOT NULL,
+  `policy_delete` tinyint(1) NOT NULL,
+  PRIMARY KEY (`policy_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
 
 --
--- AUTO_INCREMENT cho bảng `competence`
+-- Cấu trúc bảng cho bảng `policy_member`
 --
-ALTER TABLE `competence`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+DROP TABLE IF EXISTS `policy_member`;
+CREATE TABLE IF NOT EXISTS `policy_member` (
+  `member_id` int(11) NOT NULL,
+  `policy_id` int(11) NOT NULL,
+  PRIMARY KEY (`member_id`,`policy_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
 
 --
--- AUTO_INCREMENT cho bảng `config`
+-- Cấu trúc bảng cho bảng `products`
 --
-ALTER TABLE `config`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
---
--- AUTO_INCREMENT cho bảng `members`
---
-ALTER TABLE `members`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT cho bảng `product`
---
-ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE IF NOT EXISTS `products` (
+  `product_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `product_view` int(11) NOT NULL,
+  `product_star` tinyint(4) NOT NULL,
+  `product_deltail` text COLLATE utf8_unicode_ci NOT NULL,
+  `produtc_day` date NOT NULL,
+  `product_options` text COLLATE utf8_unicode_ci NOT NULL,
+  `product_num_remai` int(11) NOT NULL,
+  `product_num_sold` int(11) NOT NULL,
+  `product_price` decimal(10,0) NOT NULL,
+  `product_sale` tinyint(4) NOT NULL,
+  `product_image` text COLLATE utf8_unicode_ci NOT NULL,
+  `product_status` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `employee_id` int(11) NOT NULL,
+  `categorie_id` int(11) NOT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
