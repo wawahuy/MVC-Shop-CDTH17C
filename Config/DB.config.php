5b3252735ee7baa9e2b8511596b066b48331e798
wawahuy <kakahuy99@gmail.com>
2019-06-06 20:36:06 +0700

    .

--- Config/DB.config.php
+++ Config/DB.config.php
@@ -1,9 +1,14 @@
 <?php
-    define("HOSTDB", "127.0.0.1");
+    // define("HOSTDB", "127.0.0.1");
+    // define("USERDB", "root");
+    // define("PASSDB", "");
+    // define("NAMEDB", "qlbanhang");
+    // define("PORTDB", 3306);
+
+    define("HOSTDB", getenv("MYSQL_SERVICE_HOST"));
     define("USERDB", "username");
     define("PASSDB", "password");
     define("NAMEDB", "qlbanhang");
-    define("PORTDB", 3306);
-
+    define("PORTDB", getenv("MYSQL_SERVICE_PORT"));
 
 ?>
