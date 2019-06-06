c9e346f663c69af33b507c915ccf71aa0b0cebdd
wawahuy <kakahuy99@gmail.com>
2019-06-06 20:00:15 +0700

    openshift

--- Config/DB.config.php
+++ Config/DB.config.php
@@ -1,7 +1,7 @@
 <?php
-    define("HOSTDB", "127.0.0.1");
-    define("USERDB", "root");
-    define("PASSDB", "");
+    define("HOSTDB", "localhost");
+    define("USERDB", "username");
+    define("PASSDB", "password");
     define("NAMEDB", "qlbanhang");
 
 ?>
