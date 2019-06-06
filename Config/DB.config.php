e72618a3cf9231c72865912b6ee8446fc1b45c17
wawahuy <kakahuy99@gmail.com>
2019-06-06 20:19:05 +0700

    Rechange

--- Config/DB.config.php
+++ Config/DB.config.php
@@ -1,14 +1,14 @@
 <?php
-    // define("HOSTDB", "127.0.0.1");
-    // define("USERDB", "root");
-    // define("PASSDB", "");
-    // define("NAMEDB", "qlbanhang");
-    // define("PORTDB", 3306);
+    define("HOSTDB", "127.0.0.1");
+    define("USERDB", "root");
+    define("PASSDB", "");
+    define("NAMEDB", "qlbanhang");
+    define("PORTDB", 3306);
 
-    define("HOSTDB", getenv('OPENSHIFT_MYSQL_DB_HOST'));
-    define("USERDB", getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
-    define("PASSDB", getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-    define("NAMEDB", getenv('OPENSHIFT_APP_NAME'));
-    define("PORTDB", getenv('OPENSHIFT_MYSQL_DB_PORT'));
+    // define("HOSTDB", getenv('OPENSHIFT_MYSQL_DB_HOST'));
+    // define("USERDB", getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+    // define("PASSDB", getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+    // define("NAMEDB", getenv('OPENSHIFT_APP_NAME'));
+    // define("PORTDB", getenv('OPENSHIFT_MYSQL_DB_PORT'));
 
 ?>
